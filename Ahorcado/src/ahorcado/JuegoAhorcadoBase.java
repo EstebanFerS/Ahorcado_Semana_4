e4994cf6901b3909318139c61c8b5943ea1a1a0c
EstebanFerS <estebanfernandez865@gmail.com>
2025-11-07 08:44:21 -0600

    Juego Ahorcado Base

--- Ahorcado/src/ahorcado/JuegoAhorcadoBase.java
+++ Ahorcado/src/ahorcado/JuegoAhorcadoBase.java
@@ -10,13 +10,85 @@ import java.util.ArrayList;
  *
  * @author esteb
  */
-public abstract class JuegoAhorcadoBase implements JuegoAhorcado{
+public abstract class JuegoAhorcadoBase implements JuegoAhorcado {
+
     protected String palabraSecreta;
     protected String palabraActual;
     protected int intentos;
     protected int limiteIntentos = 6;
     protected ArrayList<Character> letrasUsadas;
     protected ArrayList<String> figuraAhorcado;
-    
-    
+
+    protected JuegoAhorcadoBase() {
+        crearFigura();
+        intentos = limiteIntentos;
+    }
+
+    protected void crearFigura() {
+        figuraAhorcado.clear();
+        figuraAhorcado.add("  _______\n"
+                + " |/      |\n"
+                + " |\n"
+                + " |\n"
+                + " |\n"
+                + " |\n"
+                + "|"
+        );
+
+        figuraAhorcado.add("  _______\n"
+                + " |/      |\n"
+                + " |      ( )\n"
+                + " |\n"
+                + " |\n"
+                + " |\n"
+                + "|"
+        );
+        figuraAhorcado.add(
+                "  _______\n"
+                + " |/      |\n"
+                + " |      ( )\n"
+                + " |       |\n"
+                + " |\n"
+                + " |\n"
+                + "|"
+        );
+
+        figuraAhorcado.add(
+                "  _______\n"
+                + " |/      |\n"
+                + " |      ( )\n"
+                + " |      /|\n"
+                + " |\n"
+                + " |\n"
+                + "|"
+        );
+
+        figuraAhorcado.add(
+                "  _______\n"
+                + " |/      |\n"
+                + " |      ( )\n"
+                + " |      /|\\\n"
+                + " |\n"
+                + " |\n"
+                + "|"
+        );
+
+        figuraAhorcado.add(
+                "  _______\n"
+                + " |/      |\n"
+                + " |      ( )\n"
+                + " |      /|\\\n"
+                + " |      / \\\n"
+                + " |\n"
+                + "|"
+        );
+
+    }
+
+    protected abstract void actualizarPalabraActual(char letra);
+
+    protected abstract void verificarLetra(char letra);
+
+    protected abstract boolean hasGanado();
+
 }
